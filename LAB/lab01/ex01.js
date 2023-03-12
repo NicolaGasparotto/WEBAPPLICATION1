9b98ea35f665df3c835473c5d14eac5e929fa8e3
Nicola Gasparotto <s258542@studenti.polito.it>
2023-03-12 11:38:15 +0100

    commit

--- LAB/lab01/ex01.js
+++ LAB/lab01/ex01.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import dayjs from 'dayjs';
+const dayjs = require("dayjs");
 
 // obj Film
 function Film(id, title, favorite=false, date=null, score=null){
@@ -13,21 +13,20 @@ function Film(id, title, favorite=false, date=null, score=null){
 
 function printDate(date){
     if(date != null)
-        return date.format('LL'); 
+        return date.format('MMMM D, YYYY'); 
     else 
         return '<not defined>';
 }
 
 function printFilm(film){
-    console.log("****  Llist of Films  ****");
-    console.log('Id: ${film.id}, Title: ${film.title},',
-    'Favorite: ${film.favorite}, Watch date: ${printDate(film.date)}, Score: ${film.score}');
+    console.log(`Id: ${film.id}, Title: ${film.title},`,
+    `Favorite: ${film.favorite}, Watch date: ${printDate(film.date)}, Score: ${film.score}` );
 }
 
 function FilmLibrary(){
-    this.films = [];
-    this.addNewFilm = film => { this.films.push(film) };
-    this.print = () => { films.forEach(element => { printFilm(element) }) };
+    let films = [];
+    this.addNewFilm = film => { films.push(film) };
+    this.print = () => { console.log('***** List of films *****'); films.forEach(element => { printFilm(element) }) };
 }
 
 function main(){
@@ -38,7 +37,7 @@ function main(){
     let f4 = new Film(4, 'Pulp Finction', false, null);
     let f5 = new Film(5, 'Pulp Finction', false, 'March 21, 2023', 3);
 
-    filmLibrary = new FilmLibrary();
+    let filmLibrary = new FilmLibrary();
     filmLibrary.addNewFilm(f1);
     filmLibrary.addNewFilm(f2);
     filmLibrary.addNewFilm(f3);
@@ -46,7 +45,7 @@ function main(){
     filmLibrary.addNewFilm(f5);
 
     filmLibrary.print();
-    
+
 }
 
 main();
