61e6fd894534912bfd85727adbbdf802020f25ed
Nicola Gasparotto <s258542@studenti.polito.it>
2023-03-12 11:28:02 +0100

    commit 0.3

--- LAB/lab01/ex00.js
+++ LAB/lab01/ex00.js
@@ -1,3 +1,34 @@
 'use strict';
+
 // https://github.com/polito-WA1-AW1-2022/lab1-node/
-console.log('Hello world!');
+// ^^^^ SOLUZIONI OFFICIALI LAB 1 21/22 ^^^^
+// console.log('Hello world!');
+
+function transformStrArr(strList){
+    let tmp;
+    function transformStr(str){
+        if(str.length > 1)
+            str = str.substring(0,2) + str.substring(str.length-2);
+        else
+            str = '';
+        return str;
+    }
+    if(Array.isArray(strList)){
+        strList.forEach(element => {
+            tmp = transformStr(element);
+            console.log(tmp);
+        });
+    }else{
+        console.log(transformStr(strList));
+    }
+}
+
+// testing function 
+function main(){
+    let string = 'spring';
+    transformStrArr(string);
+    string = ['it', 'cat', 'Code', 'a'];
+    transformStrArr(string); 
+}
+
+main();
--- LAB/lab01/ex01.js
+++ LAB/lab01/ex01.js
@@ -1,7 +1,52 @@
 'use strict';
 
+import dayjs from 'dayjs';
+
+// obj Film
+function Film(id, title, favorite=false, date=null, score=null){
+    this.id = id;
+    this.title = title;
+    this.favorite = favorite;
+    this.date = date && dayjs(date);
+    this.score = score;
+}
+
+function printDate(date){
+    if(date != null)
+        return date.format('LL'); 
+    else 
+        return '<not defined>';
+}
+
+function printFilm(film){
+    console.log("****  Llist of Films  ****");
+    console.log('Id: ${film.id}, Title: ${film.title},',
+    'Favorite: ${film.favorite}, Watch date: ${printDate(film.date)}, Score: ${film.score}');
+}
+
+function FilmLibrary(){
+    this.films = [];
+    this.addNewFilm = film => { this.films.push(film) };
+    this.print = () => { films.forEach(element => { printFilm(element) }) };
+}
+
 function main(){
     /* Function to Testing */
+    let f1 = new Film(1, 'Pulp Finction', true, 'March 10, 2023', 5);
+    let f2 = new Film(2, 'Pulp Finction', true, 'March 17, 2023', 4);
+    let f3 = new Film(3, 'Pulp Finction', false, null);
+    let f4 = new Film(4, 'Pulp Finction', false, null);
+    let f5 = new Film(5, 'Pulp Finction', false, 'March 21, 2023', 3);
+
+    filmLibrary = new FilmLibrary();
+    filmLibrary.addNewFilm(f1);
+    filmLibrary.addNewFilm(f2);
+    filmLibrary.addNewFilm(f3);
+    filmLibrary.addNewFilm(f4);
+    filmLibrary.addNewFilm(f5);
+
+    filmLibrary.print();
+    
 }
 
 main();
